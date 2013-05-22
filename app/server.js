c9060924a3d152636cadc5533ace537b3a1203a5
Oliver Smith <contact@ollyjsmith.com>
2013-05-22 12:31:48 +0100

    refactor application a bit or something

--- app/server.js
+++ app/server.js
@@ -4,46 +4,60 @@ var osc = require( 'node-osc' ),
 	ws = require( 'socket.io' ),
 	fs = require( 'fs' );
 
-// create server
-var server = http.createServer( function( request, response ){
-	fs.readFile( __dirname + '/public/index.html', function( error, data ){
-		if( error ){
-			response.writeHead( 500 );
-			return response.end( 'Error loading index.html' );
-		}
-		response.writeHead( 200 );
-		response.end( data );
-	});
-});	
+var App = function(){
+	this.server
+	this.io;
+	this.socket;
+	this.oscServer;
+};
 
-server.listen( 3000 );
-
-// sockets setup
-var sock;
-var io = ws.listen( server );
-io.sockets.on( 'connection', function( socket ){
-	sock = socket;
-	socket.emit( 'info', { data: 'Connection made.' } );
-	socket.on( 'comm', function( data ){
-		// console.log( data );
-	});
-});
-
-// osc communication setup
-var oscServer = new osc.Server( 8000, '127.0.0.1' );
-
-oscServer.on( "message", function( msg, rinfo ){
-	switch( msg[0] ){
-		case "/position":
-			sock.emit( 'osc-position-change', { position: msg[1] } );
-		break;
-		case "/msg":
-			if( msg[1].length > 0){
-				sock.emit( 'osc-new', { url: msg[1] } );
-			} else {
-				sock.emit( 'osc-disconnect', { message: 'bye bye' } );
+App.prototype = {
+	begin: function(){
+		this.server = http.createServer( this.serverRequest );
+		this.server.listen( 3000 );
+		this.io = ws.listen( this.server, { log: false } );
+		this.oscServer = new osc.Server( 8000, '127.0.0.1' );
+		this.socketsListen();
+		this.oscListen();
+	},
+	serverRequest: function( request, response ){
+		var that = this;
+		fs.readFile( __dirname + '/public/index.html', function( error, data ){
+			if( error ){
+				response.writeHead( 500 );
+				return response.end( 'Error loading index.html' );
 			}
-		break;
+			response.writeHead( 200 );
+			response.end( data );
+		});
+	},
+	socketsListen: function(){
+		var that = this;
+		this.io.sockets.on( 'connection', function( socket ){
+			that.socket = socket;
+			that.socket.emit( 'info', { connected: true } );			
+		});
+	},
+	oscListen: function(){
+		var that = this;
+		this.oscServer.on( "message", function( msg, rinfo ){
+			switch( msg[0] ){
+				case "/position":
+					that.socket.emit( 'osc-position-change', { position: msg[1] } );
+				break;
+				case "/msg":
+					if( msg[1].length > 0){
+						that.socket.emit( 'osc-new', { url: msg[1] } );
+					} else {
+						that.socket.emit( 'osc-disconnect', { message: 'bye bye' } );
+					}
+				break;
+			}
+		});
 	}
-	
-});
+}
+
+
+var interactiveBrowser = new App();
+
+interactiveBrowser.begin();
