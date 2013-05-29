60f8085ddb13c5198b656132628bab5968ef6e37
Tom Lynch <me@unknowndomain.co.uk>
2013-05-29 20:53:24 +0100

    Switched to single quotes.

--- javascript/files.js
+++ javascript/files.js
@@ -2,9 +2,9 @@ var fs = require('fs');
 
 function getStudent( url, callback ) {
 	var e;
-	var regex = "^http://open.gdnm.org/([a-z-]+)$";
-	var webroot = "public/";
-	var directory = "data/";
+	var regex = '^http://open.gdnm.org/([a-z-]+)$';
+	var webroot = 'public/';
+	var directory = 'data/';
 	var textToParagraphs = function( string ){
 		if( string ){
 			var sections = string.split('\n');
@@ -36,78 +36,78 @@ function getStudent( url, callback ) {
 
 					// Filter out .DS_Store and directories
 					 files = files.filter( function ( file ) {
-						 if ( file == ".DS_Store" )
+						 if ( file == '.DS_Store' )
 						 					 return false;
-						 if ( file == "tag.png" )
+						 if ( file == 'tag.png' )
 						 					 return false;
-						return fs.statSync( webroot + directory + student + "/" + file ).isFile();
+						return fs.statSync( webroot + directory + student + '/' + file ).isFile();
 					} );
 	
 					// Report any errors
 					if ( e ) {
-						console.log( "Error: " + e );
+						console.log( 'Error: ' + e );
 					} else {
 				
 						// Create a JSON object
 						var json = new Object();
 				
 						// Process a name from the directory name
-						json["name"] = "";
+						json['name'] = '';
 				
-						for ( part in student.split( "-" ) ) {
-							part = student.split( "-" )[part];
-							json["name"] += part.substr( 0, 1 ).toUpperCase() + part.substr( 1 ) + " ";
+						for ( part in student.split( '-' ) ) {
+							part = student.split( '-' )[part];
+							json['name'] += part.substr( 0, 1 ).toUpperCase() + part.substr( 1 ) + ' ';
 						}
 				
-						json["name"] = json["name"].trim();
+						json['name'] = json['name'].trim();
 				
 						// Process a URL from the directory name
-						json["url"] = "http://open.gdnm.org/" + student;
+						json['url'] = 'http://open.gdnm.org/' + student;
 								
 						// Process a tag from the directory name
-						json["tag"] = directory + student + "/tag.png";
+						json['tag'] = directory + student + '/tag.png';
 												
 						// Create an array of media items
-						json["media"] = new Array();
+						json['media'] = new Array();
 
 						// Fill array
 						for ( file in files  ) {
 					
 							// If file is an image then add image: filename
-							if ( files[file].substr( -3, 3 ) == "jpg" | files[file].substr( -3, 3 ) == "png" | files[file].substr( -4, 4 ) == "jpeg" | files[file].substr( -3, 3 ) == "gif" ) {
+							if ( files[file].substr( -3, 3 ) == 'jpg' | files[file].substr( -3, 3 ) == 'png' | files[file].substr( -4, 4 ) == 'jpeg' | files[file].substr( -3, 3 ) == 'gif' ) {
 								var item = new Object();
-								item["image"] = directory + student + "/" + files[file];
-								json["media"].push( item );
+								item['image'] = directory + student + '/' + files[file];
+								json['media'].push( item );
 
 							// If file is a video then add video: filename
-							} else if ( files[file].substr( -3, 3 ) == "mp4" ) { 
+							} else if ( files[file].substr( -3, 3 ) == 'mp4' ) { 
 								var item = new Object();
-								item["video"] = student + "/" + files[file];
-								json["media"].push( item );
+								item['video'] = student + '/' + files[file];
+								json['media'].push( item );
 							}
 						}
 				
 						// Process a bio from the directory name
-						fs.exists( webroot + directory + student + "/blurb.txt", function ( exists ) {
+						fs.exists( webroot + directory + student + '/blurb.txt', function ( exists ) {
 							if ( exists ) {
-								fs.readFile( webroot + directory + student + "/blurb.txt", "utf8", function ( e, data ) {
+								fs.readFile( webroot + directory + student + '/blurb.txt', 'utf8', function ( e, data ) {
 									if ( ! e ) {
-										json["blurb"] = {'text': textToParagraphs( data ) };
+										json['blurb'] = {'text': textToParagraphs( data ) };
 									} else {
-										console.log( "Blurb error" );
+										console.log( 'Blurb error' );
 									}
 							
 									// Output data
-									if ( typeof callback  === "function" ) {
+									if ( typeof callback  === 'function' ) {
 										callback( e, json );
 									}
 								});
 							} else {
-								json["blurb"] = "";
-								console.log( "No blurb" );
+								json['blurb'] = '';
+								console.log( 'No blurb' );
 
 								// Output data
-								if ( typeof callback  === "function" ) {
+								if ( typeof callback  === 'function' ) {
 									callback( e, json );
 								}
 							}
@@ -117,8 +117,8 @@ function getStudent( url, callback ) {
 				
 			// Output error
 			} else {
-				e = "Error: No such student directory exists."
-				if ( typeof callback  === "function" ) {
+				e = 'Error: No such student directory exists.'
+				if ( typeof callback  === 'function' ) {
 					callback( e, null );
 				}
 			}
@@ -126,8 +126,8 @@ function getStudent( url, callback ) {
 
 	// Output error
 	} else {
-		e = "Error: URL format invalid."
-		if ( typeof callback  === "function" ) {
+		e = 'Error: URL format invalid.'
+		if ( typeof callback  === 'function' ) {
 			callback( e, null );
 		}
 	}
